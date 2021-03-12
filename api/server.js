dc3eef15fed8219694c647ccc1e47a60d99727a9
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-03-12 13:41:15 -0700

    MVP

--- api/server.js
+++ api/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const server = express();
 
 const actionsRouter = require('./actions/actions-router');
-
+const projectsRouter = require('./projects/projects-router');
 server.use(express.json());
 server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require('./api/server');
 
 const actionsRouter = require('./api/actions/actions-router');
+const projectsRouter = require('./api/projects/projects-router');
 
 server.use(actionsRouter);
+server.use(projectsRouter);
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
