27391e925b4abb54efcd9c83342019748d424ddf
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-03-12 10:59:01 -0700

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,45 @@
 // Write your "actions" router here!
+const express = require('express');
+
+const Action = require('./actions-model');
+
+
+const router = express.Router();
+
+// GET /api/actions
+router.get("/", async (req, res, next) => {
+    try {
+        const actions = await Action.get();
+        res.status(200).json(actions);
+    } catch (err) {next(err)}
+});
+
+// GET /api/actions/:id
+router.get("/:id", async (req, res) => {
+    try {
+        const action = await Action.get(req.params.id);
+        if(!action){
+            res.status(404).json({ message: "action not found" })
+        } else {
+            res.status(200).json(action);
+        }
+    } catch (err){
+        res.status(500).json({message: 'Action could not be retrieved'});
+    } 
+});
+
+// POST /api/actions
+router.post("/", async (req, res) => {
+    const newAction = await Action.insert(req.body)
+    try {
+        if(!req.body.description || !req.body.project_id){
+            res.status(400).json({message: 'missing data'});
+        } else {
+            res.status(201).json(newAction);
+        }
+    } catch (err){
+        res.status(500).json({message: 'Action could not be retrieved'});
+    } 
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const server = express();
+
+const actionsRouter = require('./actions/actions-router');
+
+server.use(express.json());
+server.use('/api/actions', actionsRouter);
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
