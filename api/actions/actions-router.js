57eeb43ca01439d384f15e9eb3d00539c72816ce
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-03-12 12:07:54 -0700

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const Action = require('./actions-model');
 
+const { validateActionId, validateAction } = require('../middleware/actionMiddleware');
+
 
 const router = express.Router();
 
@@ -15,31 +17,41 @@ router.get("/", async (req, res, next) => {
 });
 
 // GET /api/actions/:id
-router.get("/:id", async (req, res) => {
-    try {
-        const action = await Action.get(req.params.id);
-        if(!action){
-            res.status(404).json({ message: "action not found" })
-        } else {
-            res.status(200).json(action);
-        }
-    } catch (err){
-        res.status(500).json({message: 'Action could not be retrieved'});
-    } 
+router.get("/:id", validateActionId, async (req, res) => {
+    const { id } = req.params;
+    const action = await Action.get(id);
+    res.status(200).json(action);
 });
 
 // POST /api/actions
-router.post("/", async (req, res) => {
-    const newAction = await Action.insert(req.body)
-    try {
-        if(!req.body.description || !req.body.project_id){
-            res.status(400).json({message: 'missing data'});
-        } else {
-            res.status(201).json(newAction);
-        }
-    } catch (err){
-        res.status(500).json({message: 'Action could not be retrieved'});
-    } 
+router.post("/", validateAction, async (req, res) => {
+    const newAction = await Action.insert(req.body);
+    res.status(201).json(newAction);
 })
+
+// PUT /api/actions/:id
+router.put("/:id", async (req, res) => {
+    if(!req.body) {
+        res.status(400).json({ message: "All fields required" })
+    } 
+    await Action.update(req.params.id)
+        .then((action) => {
+            if (action) {
+                res.status(202).json(action)
+            } else {
+                res.status(404).json({
+                    message: "The action with the specified ID does not exist"
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "The action could not be modified."
+            })
+        })
+})
+    
+
 
 module.exports = router;
